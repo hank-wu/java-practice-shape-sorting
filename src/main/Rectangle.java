8bd0e19813a1f2c970108ab81b1bef2830062b14
hank-wu <HankWu0203gmail.com>
2021-11-04 21:24:55 +0900

    today'parts

---
+++ src/main/Rectangle.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Rectangle implements IShape{
+    private double length;
+    private double width;
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area(){
+        return length * width;
+    }
+
+    @Override
+    public double perimeter(){
+        return (length + width) * 2;
+    }
+}
