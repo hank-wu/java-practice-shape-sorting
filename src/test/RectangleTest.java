47f55130dff1599b6d318536d14a55ffc91ccd72
hank-wu <HankWu0203gmail.com>
2021-11-09 17:19:13 +0900

    add exception in each shape

--- src/test/RectangleTest.java
+++ src/test/RectangleTest.java
@@ -1,7 +1,9 @@
 package test;
 
+import main.IShape;
 import main.Rectangle;
 
+import main.ShapeException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,5 +28,31 @@ class RectangleTest {
     @DisplayName("Rectangleオブジェクトの周長を検証")
     void perimeter() {
         assertEquals(22, rectangle.perimeter(), 0.000001);
+    }
+
+    @Test
+    @DisplayName("Rectangleオブジェクトは長さが-1によって、作り失敗")
+    void rectangleWithInvalidLength() {
+        IShape testShape;
+        String errorMessage = "";
+        try{
+            testShape = new Rectangle(-1, 5);
+        }catch (ShapeException e){
+            errorMessage = e.getMessage();
+        }
+        assertEquals("Not valid Rectangle!", errorMessage);
+    }
+
+    @Test
+    @DisplayName("Rectangleオブジェクトは幅が-1によって、作り失敗")
+    void rectangleWithInvalidWidth() {
+        IShape testShape;
+        String errorMessage = "";
+        try{
+            testShape = new Rectangle(5, -1);
+        }catch (ShapeException e){
+            errorMessage = e.getMessage();
+        }
+        assertEquals("Not valid Rectangle!", errorMessage);
     }
 }
--- src/test/TriangleTest.java
+++ src/test/TriangleTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import main.IShape;
+import main.ShapeException;
 import main.Triangle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,5 +26,31 @@ class TriangleTest {
     @DisplayName("Triangleオブジェクトの周長を検証")
     void perimeter() {
         assertEquals(12, triangle.perimeter(),0.000001);
+    }
+
+    @Test
+    @DisplayName("Triangleオブジェクトは辺の一つが-1によって、作り失敗")
+    void triangleWithMinusLength() {
+        IShape testShape;
+        String errorMessage = "";
+        try{
+            testShape = new Triangle(-1, 3,4);
+        }catch (ShapeException e){
+            errorMessage = e.getMessage();
+        }
+        assertEquals("There is a input length less than zero ,so it's not a valid Triangle!", errorMessage);
+    }
+
+    @Test
+    @DisplayName("Triangleオブジェクトは三つの辺が条件を満たさないので、作り失敗")
+    void triangleWithWrongLengthCondition() {
+        IShape testShape;
+        String errorMessage = "";
+        try{
+            testShape = new Triangle(1, 1,100);
+        }catch (ShapeException e){
+            errorMessage = e.getMessage();
+        }
+        assertEquals("The relation of the three length doesn't satisfy the condition of Triangle!", errorMessage);
     }
 }
